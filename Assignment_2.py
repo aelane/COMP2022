1506ae9265d0a800fca61714df48c3406acb0663
Annie Lane <annieel94@gmail.com>
2014-05-05 17:19:59 +1000

    Added Psuedocode in comments and thoughts about string processing

--- Assignment_2.py
+++ Assignment_2.py
@@ -5,10 +5,17 @@
 #Hi!
 
 #----------------------------Input Processing Functions----------------------------#
+#This also includes functions necessary to read input .txt files
 
+#Read file
+#file_object = open(filename, mode) where file_object is the variable to put the file object.
+#mode is just 'r' since we are just reading
+#file_object.close() in order to close the file
 
+#Remove spaces and line breaks (CRs)
+#supposedly that's just string.strip() but we can look that up
 
-
+#Add $ to end of string to indicate end of string
 
 
 
@@ -16,10 +23,22 @@
 
 #---------------------------Parse Table Functions----------------------------------#
 
+#We need to decide on how we want to represent functions
 
 
 
+#--------------------------Parser Psuedo-Code (Slide 28)---------------------------------#
 
-
+##loop
+##    T = symbol on top of stack
+##    C = current input symbol
+##    if T == C == $ then accept
+##    elif T is a terminal or T = $ then
+##        if T == c then pop T, consume the input c
+##        else error
+##    elif P[T,c] = alpha (means if the entry in table contains the production T -> alpha)
+##        pop T and push the symbols of alpha on the stack in reverse order
+##        else error
+##endloop
 
 #--------------------------Function Calls------------------------------------------#
